6a8b0f7dc7c5bf9ed156ec118ffda6198481dc36
Ricardo Fleury <ricardofleury@ricardos-mbp.gateway.pace.com>
2015-03-11 19:32:24 -0500

    Create migration tables for users purchases and shirts

--- db/migrate/20150311192544_create_shirts.rb
+++ db/migrate/20150311192544_create_shirts.rb
@@ -1,4 +1,7 @@
 class CreateShirts < ActiveRecord::Migration
   def change
+    create_table  :shirts do |t|
+      t.belongs_to :designer
+    end
   end
 end
--- db/migrate/20150311192553_create_purchases.rb
+++ db/migrate/20150311192553_create_purchases.rb
@@ -1,4 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration
   def change
+    create_table :purchases do |t|
+      t.belongs_to  :shirt
+      t.belongs_to  :purchaser
+    end
   end
 end
